e0b6da2c3c66a8d877fbc6f5c44a6caf650d7b71
Samuele Caprioli <samuele.caprioli@elmec.it>
2018-08-02 13:58:17 +0200

    fix#5 Put tooltip into body and anchor to parent element by calculating the correct position with getBoundingClientRect

--- dist/simple-tooltip.common.js
+++ dist/simple-tooltip.common.js
@@ -129,83 +129,100 @@ var external_commonjs_vue_commonjs2_vue_root_Vue_default = /*#__PURE__*/__webpac
     createTooltip(el, binding)
   },
   update: function (el, binding) {
-    let oldTooltipElements = el.querySelectorAll('.directive-tooltip-selector')
-    el.removeChild(oldTooltipElements[0])
-    el.removeChild(oldTooltipElements[1])
+    let oldTooltipElements = document.getElementsByClassName('directive-tooltip-selector')
+    if (oldTooltipElements[0].parentElement == el) {
+      el.removeChild(oldTooltipElements[0])
+    }
+    if (oldTooltipElements[1].parentElement == el) {
+      el.removeChild(oldTooltipElements[1])
+    }
     createTooltip(el, binding)
   }
 }));
 function createTooltip(el, binding) {
-  el.style.position = 'relative'
   let tooltipBox = document.createElement('div')
   let tooltipArrow = document.createElement('div')
-  let tooltipText = document.createElement('div')
+  let tooltipText = document.createElement('span')
+
   tooltipText.innerHTML = binding.value
-  console.log(binding.value)
-  switch (binding.arg) {
-    case 'bottom':
-      tooltipBox.style.top = '100%'
-      tooltipBox.style.left = '50%'
-      tooltipBox.style.transform = 'translate(-50%, 10px)'
-      tooltipArrow.style.top = '100%'
-      tooltipArrow.style.left = '50%'
-      tooltipArrow.style.borderColor = 'transparent transparent black transparent'
-      tooltipArrow.style.marginLeft = '-5px'
-      break
-    case 'left':
-      tooltipBox.style.right = '100%'
-      tooltipBox.style.top = '50%'
-      tooltipBox.style.transform = 'translate(-10px, -50%)'
-      tooltipArrow.style.right = '100%'
-      tooltipArrow.style.top = '50%'
-      tooltipArrow.style.borderColor = 'transparent transparent transparent black'
-      tooltipArrow.style.marginTop = '-5px'
-      break
-    case 'right':
-      tooltipBox.style.left = '100%'
-      tooltipBox.style.top = '50%'
-      tooltipBox.style.transform = 'translate(10px, -50%)'
-      tooltipArrow.style.left = '100%'
-      tooltipArrow.style.top = '50%'
-      tooltipArrow.style.borderColor = 'transparent black transparent transparent'
-      tooltipArrow.style.marginTop = '-5px'
-      break
-    default:
-      tooltipBox.style.bottom = '100%'
-      tooltipBox.style.left = '50%'
-      tooltipBox.style.transform = 'translate(-50%, -10px)'
-      tooltipArrow.style.bottom = '100%'
-      tooltipArrow.style.left = '50%'
-      tooltipArrow.style.borderColor = 'black transparent transparent transparent'
-      tooltipArrow.style.marginLeft = '-5px'
-      break
-  }
+
   tooltipBox.style.position = 'absolute'
-  tooltipBox.style.minWidth = '100px'
-  tooltipBox.style.zIndex = '2'
+  tooltipBox.style.zIndex = '90'
   tooltipBox.style.backgroundColor = 'black'
   tooltipBox.style.color = '#fff'
-  tooltipBox.style.textAlign = 'center'
-  tooltipBox.style.padding = '5px'
+  tooltipBox.style.padding = '5px 20px 5px 20px'
   tooltipBox.style.borderRadius = '6px'
   tooltipBox.style.visibility = 'hidden'
+  tooltipBox.style.fontSize = '12px'
+  tooltipBox.style.maxWidth = '320px'
 
   tooltipArrow.style.content = ''
   tooltipArrow.style.position = 'absolute'
-  tooltipArrow.style.zIndex = '2'
+  tooltipArrow.style.zIndex = '90'
   tooltipArrow.style.borderWidth = '5px'
   tooltipArrow.style.borderStyle = 'solid'
   tooltipArrow.style.visibility = 'hidden'
+
   tooltipBox.appendChild(tooltipText)
   tooltipBox.classList.add('directive-tooltip-selector')
   tooltipArrow.classList.add('directive-tooltip-selector')
-  el.appendChild(tooltipBox)
-  el.appendChild(tooltipArrow)
+
+  let body = document.getElementsByTagName('body')[0]
+  body.appendChild(tooltipBox)
+  body.appendChild(tooltipArrow)
   el.onmouseover = function () {
+    let scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    switch (binding.arg) {
+      case 'bottom':
+        tooltipBox.style.left = el.getBoundingClientRect().left - tooltipBox.getBoundingClientRect().width / 2 + el.getBoundingClientRect().width / 2 + scrollLeft + 'px'
+        tooltipBox.style.top = el.getBoundingClientRect().y + el.getBoundingClientRect().height + tooltipArrow.getBoundingClientRect().height / 2 + scrollTop + 'px'
+        tooltipArrow.style.left = el.getBoundingClientRect().left + el.getBoundingClientRect().width / 2 + scrollLeft + 'px'
+        tooltipArrow.style.top = el.getBoundingClientRect().y + el.getBoundingClientRect().height - tooltipArrow.getBoundingClientRect().height / 2  + scrollTop + 'px'
+        tooltipArrow.style.borderColor = 'transparent transparent black transparent'
+        tooltipArrow.style.marginLeft = '-5px'
+        break
+      case 'right':
+        tooltipBox.style.left = el.getBoundingClientRect().left + el.getBoundingClientRect().width + tooltipArrow.getBoundingClientRect().width / 2 + scrollLeft + 'px'
+        tooltipBox.style.top = el.getBoundingClientRect().y - tooltipBox.getBoundingClientRect().height / 2 + el.getBoundingClientRect().height / 2   + scrollTop + 'px'
+        tooltipArrow.style.left = el.getBoundingClientRect().left + el.getBoundingClientRect().width - tooltipArrow.getBoundingClientRect().width /2 + scrollLeft + 'px'
+        tooltipArrow.style.top = el.getBoundingClientRect().y + el.getBoundingClientRect().height / 2 + scrollTop + 'px'
+        tooltipArrow.style.borderColor = 'transparent black transparent transparent'
+        tooltipArrow.style.marginTop = '-5px'
+        break
+      case 'left':
+        tooltipBox.style.left = el.getBoundingClientRect().left - tooltipBox.getBoundingClientRect().width - tooltipArrow.getBoundingClientRect().width / 2 + scrollLeft + 'px'
+        tooltipBox.style.top = el.getBoundingClientRect().y - tooltipBox.getBoundingClientRect().height / 2 + el.getBoundingClientRect().height / 2   + scrollTop + 'px'
+        tooltipArrow.style.left = el.getBoundingClientRect().left - tooltipArrow.getBoundingClientRect().width / 2 + scrollLeft + 'px'
+        tooltipArrow.style.top = el.getBoundingClientRect().y + el.getBoundingClientRect().height / 2 + scrollTop + 'px'
+        tooltipArrow.style.borderColor = 'transparent transparent transparent black'
+        tooltipArrow.style.marginTop = '-5px'
+        break
+      default:
+        tooltipBox.style.left = el.getBoundingClientRect().left - tooltipBox.getBoundingClientRect().width / 2 + el.getBoundingClientRect().width / 2 + scrollLeft + 'px'
+        tooltipBox.style.top = el.getBoundingClientRect().y - tooltipBox.getBoundingClientRect().height - tooltipArrow.getBoundingClientRect().height / 2 + scrollTop + 'px'
+        tooltipArrow.style.left = el.getBoundingClientRect().left + el.getBoundingClientRect().width / 2 + scrollLeft + 'px'
+        tooltipArrow.style.top = el.getBoundingClientRect().y - tooltipArrow.getBoundingClientRect().height / 2 + scrollTop + 'px'
+        tooltipArrow.style.borderColor = 'black transparent transparent transparent'
+        tooltipArrow.style.marginLeft = '-5px'
+        break
+    }
     tooltipBox.style.visibility = 'visible'
     tooltipArrow.style.visibility = 'visible'
   }
   el.onmouseleave = function () {
+    tooltipBox.style.visibility = 'hidden'
+    tooltipArrow.style.visibility = 'hidden'
+  }
+  tooltipArrow.onmouseover = function () {
+    tooltipBox.style.visibility = 'visible'
+    tooltipArrow.style.visibility = 'visible'
+  }
+  tooltipBox.onmouseover = function () {
+    tooltipBox.style.visibility = 'visible'
+    tooltipArrow.style.visibility = 'visible'
+  }
+  tooltipBox.onmouseleave = function () {
     tooltipBox.style.visibility = 'hidden'
     tooltipArrow.style.visibility = 'hidden'
   }
